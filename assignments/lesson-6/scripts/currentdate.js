5b66b4c1300a3dee2247e09f87556d558d8ab5e5
Coleman_Likes <crlikes@byui.edu>
2019-02-16 18:25:02 -0700

    Added Lesson 6

---
+++ assignments/lesson-6/scripts/currentdate.js
@@ -0,0 +1,42 @@
+/*var d = new Date();
+var weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+var c = month[d.getMonth()];
+document.getElementById("date").innerHTML = n, c;
+*/
+//getmonth
+
+//add to the same get elementbyid. everything will be added to line 12
+
+//move document.getelementbyid to the bottom.
+
+var d = new Date();
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+document.getElementById("date").innerHTML = d.toLocaleDateString('en-US', options);
+/*
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+document.getElementById("year").innerHTML=n;
+
+//getmonth
+
+//add to the same get elementbyid. everything will be added to line 12
+
+//move document.getelementbyid to the bottom.*/
