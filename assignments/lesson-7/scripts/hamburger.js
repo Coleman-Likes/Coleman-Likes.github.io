670e24f26e0c083456686c16d082f22e222864c2
Coleman_Likes <crlikes@byui.edu>
2019-02-22 21:28:25 -0700

    fixed things

--- assignments/lesson-7/scripts/hamburger.js
+++ assignments/lesson-7/scripts/hamburger.js
@@ -1,9 +1,17 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+/*function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+      x.className += "responsive";
     } else {
       x.className = "topnav";
     }
   }
+*/
+
+
+
+  function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+  }
+
---
+++ assignments/lesson-7/scripts/windchill.js
@@ -0,0 +1,8 @@
+let x = parseInt(document.getElementById('somevalue').innerHTML);
+let result = (x + 10) / 2;
+document.getElementById('output').innerHTML = "The temperature is <strong>" + result.toFixed(1) + "</strong>";
+
+/*The formula to calculate the wind chill factor is 
+LaTeX: f=35.74+0.6215\:t-35.75\:s^{0.16}+0.4275\:t\:s^{0.16}
+ f = 35.74 + 0.6215 t − 35.75 s 0.16 + 0.4275 t s 0.16 
+*/
