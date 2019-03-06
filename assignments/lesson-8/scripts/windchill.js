8ca547f83d4eba6a9d2516a92d1e6fcf533f3a43
Coleman_Likes <crlikes@byui.edu>
2019-03-05 20:02:25 -0700

    new

--- assignments/lesson-8/scripts/windchill.js
+++ assignments/lesson-8/scripts/windchill.js
@@ -8,3 +8,11 @@ document.getElementById('output').innerHTML = "The temperature is <strong>" + re
 LaTeX: f=35.74+0.6215\:t-35.75\:s^{0.16}+0.4275\:t\:s^{0.16}
  f = 35.74 + 0.6215 t − 35.75 s 0.16 + 0.4275 t s 0.16 
 */
+f=35.74+0.6215
+
+:t/*temperature*/-35.75
+:s/*speed*/^{0.16}+0.4275  /*use math.pow*/
+:t
+:s^{0.16}
+ f/*fahrenheit*/ = 35.74 + 0.6215
+t − 35.75 s 0.16 + 0.4275 t s 0.16 
