000c1250ea129a8421f6ebffd56177baccfee8f4
Coleman_Likes <crlikes@byui.edu>
2019-03-09 23:31:02 -0700

    added json data

---
+++ assignments/lesson-9_weather_site_json/scripts/town_json_data.js
@@ -0,0 +1,50 @@
+
+//JSON retrieve information from URL.
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+    var townData = request.response;
+    //populateHeader(townData);
+    newTowns(townData);
+}
+
+function newTowns(jsonObj) {
+    var towns = jsonObj['towns'];
+    //header.appendChild(myH1);
+
+    for (var i = 0; i < heroes.length; i++) {
+
+        var article = document.createElement('article');
+        var h2 = document.createElement('h2');
+
+
+        h2.textContent = towns[i].name;
+
+
+        var myPara1 = document.createElement('p');
+
+        var myPara2 = document.createElement('p');
+
+        var myPara3 = document.createElement('p');
+
+        var myList = document.createElement('ul');
+
+
+        section.appendChild(myArticle);
+
+
+        myPara1.textContent = towns[i].motto;
+        myPara2.textContent = 'yearfounded: ' + towns[i].yearFounded;
+        myPara3.textContent = 'currentpopulation: ' + towns[i].currentPopulation;
+        myPara4.textContent = 'averagerainfall:' + towns[i].averageRainfall;;
+
+
+
+        document.getElementById('towndataid').appendChild(article);
+    }
+}
+        
