dcc9d456295cc47be40c6c3a837b679ad7d57df1
Coleman_Likes <crlikes@byui.edu>
2019-03-11 20:10:42 -0600

    Working

--- assignments/lesson-9_weather_site_json/scripts/town_json_data.js
+++ assignments/lesson-9_weather_site_json/scripts/town_json_data.js
@@ -10,39 +10,34 @@ request.onload = function () {
     var townData = request.response;
     //populateHeader(townData);
     newTowns(townData);
+    //console.log(townData);
 }
 
 function newTowns(jsonObj) {
-    var towns = jsonObj['towns'];
-    //header.appendChild(myH1);
-
-    for (var i = 0; i < towns.length; i++) {
-
+    var idahotowns = jsonObj['towns'];
+//console.log(idahotowns);
+    for (var i = 0; i < idahotowns.length; i++) {
         var article = document.createElement('article');
         var h2 = document.createElement('h2');
-
-
-        h2.textContent = towns[i].name;
-
+        h2.textContent = idahotowns[i].name;
 
         var myPara1 = document.createElement('p');
-
         var myPara2 = document.createElement('p');
-
         var myPara3 = document.createElement('p');
-
         var myPara4 = document.createElement('p');
 
+        //section.appendChild(article);
+        //section id ="towndataid"
+        //document.getElementById('towndataid').appendChild(article);
 
-        section.appendChild(article);
-
-
-        myPara1.textContent = towns[i].motto;
-        myPara2.textContent = 'yearfounded: ' + towns[i].yearFounded;
-        myPara3.textContent = 'currentpopulation: ' + towns[i].currentPopulation;
-        myPara4.textContent = 'averagerainfall:' + towns[i].averageRainfall;;
-
-
+        myPara1.textContent = idahotowns[i].motto;
+        myPara2.textContent = 'yearfounded: ' + idahotowns[i].yearFounded;
+        myPara3.textContent = 'currentpopulation: ' + idahotowns[i].currentPopulation;
+        myPara4.textContent = 'averagerainfall:' + idahotowns[i].averageRainfall;
+        article.appendChild(myPara1);
+        article.appendChild(myPara2);
+        article.appendChild(myPara3);
+        article.appendChild(myPara4);
 
         document.getElementById('towndataid').appendChild(article);
     }
