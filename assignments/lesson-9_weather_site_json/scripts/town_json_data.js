4b9bfcd770e99752620ce03a058465b8c002912a
Coleman_Likes <crlikes@byui.edu>
2019-03-12 22:59:05 -0600

    new

--- assignments/lesson-9_weather_site_json/scripts/town_json_data.js
+++ assignments/lesson-9_weather_site_json/scripts/town_json_data.js
@@ -13,7 +13,6 @@ request.onload = function () {
 }
 
     function newTowns(jsonObj) {
-    //console.log (jsonObj);
     var idahotowns = jsonObj['towns'];
     //console.log(idahotowns);
     for (var i = 0; i < idahotowns.length; i++) {
@@ -24,20 +23,32 @@ request.onload = function () {
         h2.textContent = idahotowns[i].name; //set h2 tag name to idahotowns
         article.appendChild(h2);
 
+       // towns.name = 'Soda Springs';
+        //towns.name = 'Fish Haven';
+
+        //Preston, Soda Springs, Fish Haven.
         var myPara1 = document.createElement('p');
         var myPara2 = document.createElement('p');
         var myPara3 = document.createElement('p');
         var myPara4 = document.createElement('p');
+        //var myPara5 = document.createElement('p');  events
 
         myPara1.textContent = idahotowns[i].motto;
         myPara2.textContent = 'yearfounded: ' + idahotowns[i].yearFounded;
         myPara3.textContent = 'currentpopulation: ' + idahotowns[i].currentPopulation;
         myPara4.textContent = 'averagerainfall:' + idahotowns[i].averageRainfall;
+
+        //"name": "Preston",
+        //myPara5.textContent =  idahotowns[i].events;  events
+        //myPara5.textContent = 'Preston' + idahotowns[i].Preston;
+        //towns.name = "Preston" + idahotowns[i].motto;
+        //article.appendChild(towns.name);
        
         article.appendChild(myPara1);
         article.appendChild(myPara2);
         article.appendChild(myPara3);
         article.appendChild(myPara4);
+        //article.appendChild(myPara5);  events
 
         document.getElementById('towndataid').appendChild(article);
     }
