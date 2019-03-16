bf8e41d4ec40a5f96186ede3d6ad9208d33e808d
Coleman_Likes <crlikes@byui.edu>
2019-03-16 13:35:19 -0600

    new

---
+++ assignments/lesson-10/scripts/town_json_data.js
@@ -0,0 +1,54 @@
+var section = document.querySelector("section");
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var request = new XMLHttpRequest();
+
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+    var townData = request.response;
+    newTowns(townData);
+}
+
+    function newTowns(jsonObj) {
+         var idahotowns = jsonObj["towns"];
+
+    for (var i = 0; i < idahotowns.length; i++) {
+    if (idahotowns[i].name == "Preston" || idahotowns[i].name == "Soda Springs" 
+    || idahotowns[i].name == "Fish Haven") { 
+    //console.log(idahotowns); view log in console.
+
+        var article = document.createElement("section");
+        article.setAttribute("class", "idahoarticle" + i); /*Each article has the same class*/
+        //article.setAttribute("id", idahotowns[i].name + "article"); each article has a unique id.
+
+       //can be '' or "".
+        var myH2 = document.createElement('h2'); 
+        var myPara1 = document.createElement('p');
+        var myPara2 = document.createElement('p');
+        var myPara3 = document.createElement('p');
+        var myPara4 = document.createElement('p');
+        //var myPara5 = document.createElement('p'); events data
+
+        myH2.textContent = idahotowns[i].name; //set h2 tag name to idahotowns
+        myPara1.textContent = idahotowns[i].motto;
+        myPara2.textContent = 'yearfounded: ' + idahotowns[i].yearFounded;
+        myPara3.textContent = 'currentpopulation: ' + idahotowns[i].currentPopulation;
+        myPara4.textContent = 'averagerainfall:' + idahotowns[i].averageRainfall;
+        //myImage.src ="images/townname.jpg";
+        //myPara5.textContent = 'Events:' + towns[i].events; 
+        //for future events data
+
+        article.appendChild(myH2);
+        article.appendChild(myPara1);
+        article.appendChild(myPara2);
+        article.appendChild(myPara3);
+        article.appendChild(myPara4);
+        section.appendChild(article);
+        //myArticle.appendChild(myPara5); events data
+
+
+       // document.getElementById('towndataid').appendChild(article);
+    }
+  }
+    }
---
+++ assignments/lesson-10/scripts/windchill.js
@@ -0,0 +1,21 @@
+//Convert variable into an integer
+//id span name is temp and speed.
+var temperature = parseInt(document.getElementById
+    ('temp').innerHTML);
+var windspeed = parseInt(document.getElementById
+    ('speed').innerHTML);
+
+//Use fullname variable so it does not conflict.
+//Windchill calculation using variables
+//Use windchill formula. Use javascript math.
+windchill = 35.74 + 0.6215 * temperature 
++ (0.4275 * temperature - 35.75) * Math.pow(windspeed, 0.16);
+
+ //Fixed must be a 0.
+ document.getElementById('chillout').innerHTML = 
+ + windchill.toFixed(0);
+
+ /*old codepen example
+document.getElementById('output').innerHTML =
+ "The temperature is <strong>" + result.toFixed(1) + "</strong>";
+*/
